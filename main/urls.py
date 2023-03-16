9ae83a74d6e00ffc7da6cf460b35799d1562601e
N0153128 <noisebro@hotmail.com>
2023-03-16 02:04:37 +0000

    added basic templates for main pages - index, person, about

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'Board'
+app_name = 'Main'
 urlpatterns = [
-    path('', views.main, name='board'),
+    path('', views.main, name='index'),
+    path('person', views.person, name='person'),
+    path('about', views.about, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -12,6 +12,20 @@ import constants
 def main(request):
     template = loader.get_template('main/index.html')
     context = {
+     }
+    return HttpResponse(template.render(context, request))
+
+
+def person(request):
+    template = loader.get_template('main/person.html')
+    context = {
         'data': constants,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    template = loader.get_template('main/about.html')
+    context = {
     }
     return HttpResponse(template.render(context, request))
